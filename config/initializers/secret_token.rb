f168c8c5c5ce75bcde251bce4a1f0749c5074b6d
escapada <escapada@list.ru>
2015-06-16 22:22:41 +0300

    blank refinerycms with marinerus temlate home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marinerus::Application.config.secret_token = '22386f344ce77681244862795c3ff3433c3273f433a2b8a3ca6b2458384294124b182c6aa13a501e574877cf8ede7264ff4608a0cb6be38d9c101839623bb7cc'
